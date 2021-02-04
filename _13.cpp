31be47e6c31c88f9744329046f7359237decc3a5
Your Name <ultimate03012002@gmail.com>
2021-02-04 12:17:48 +0700

    hehe

--- _13.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-int main(){
-	int x,y;
-	cout<<"Nhap vao x,y: ";
-	cin>>x>>y;
-	cout<<"Khoang cach tu diem(x,y) toi doc toa do(0,0): "<<sqrt(x*x+y*y);
-}
